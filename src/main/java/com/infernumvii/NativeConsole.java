4930eedf145fbc51b234d1dbd2a375d5e7021fde
InfernumVII <egor554shved@gmail.com>
2025-04-26 07:48:21 +0300

    first commit

---
+++ src/main/java/com/infernumvii/NativeConsole.java
@@ -0,0 +1,45 @@
+package com.infernumvii;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+public class NativeConsole {
+    private static final boolean IS_WINDOWS = System.getProperty("os.name").startsWith("Windows");
+    static {
+        if (!System.getProperty("os.name").startsWith("Windows")) {
+            throw new UnsupportedOperationException("Windows only");
+        }
+
+        try {
+            Path tempDll = Files.createTempFile("nativeconsole", ".dll");
+            try (var dllStream = NativeConsole.class.getResourceAsStream("/native/nativeconsole.dll")) {
+                Files.copy(dllStream, tempDll, StandardCopyOption.REPLACE_EXISTING);
+            }
+            System.load(tempDll.toString());
+        } catch (IOException | NullPointerException e) {
+            throw new RuntimeException("Failed to load native library", e);
+        }
+    }
+    public native static int readCharImmediately(); 
+    private NativeConsole(){
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+            int ch = readCharImmediately();
+            if (ch != -1){
+                System.out.println((char)ch);
+            }
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        
+    }
+    
+}
